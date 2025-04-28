1c17e56668f72c555173a79b23b9504aba11d978
deshan68 <arundeshan@gmail.com>
2025-04-28 12:36:29 +0530

    Feat: add GPAClass and GPAColor types for GPA classification

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,14 @@
 import { ReactElement } from "react";
+import { GPAClass, GPAColor } from "./enums";
 
 export interface ITab {
   id: string;
   label: string;
   content: JSX.Element;
   action: ReactElement;
+}
+
+export interface GPAClassMapping {
+  className: GPAClass;
+  color: GPAColor;
 }
