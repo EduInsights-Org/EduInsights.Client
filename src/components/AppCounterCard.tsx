19ddeb5b43998af3a244ae176f300ec304a6a36e
deshan68 <arundeshan@gmail.com>
2025-04-29 00:55:23 +0530

    Feat: update AppStatCard component to support optional mainTitle and improve layout

--- src/components/AppCounterCard.tsx
+++ src/components/AppCounterCard.tsx
@@ -1,27 +1,34 @@
 interface AppStatCardProps {
-  icon: React.ComponentType<{ className?: string }>;
+  icon?: React.ComponentType<{ className?: string }>;
   value: number | string;
-  title: string;
+  mainTitle?: string;
+  mainTitleClassName?: string;
+  subTitle: string;
   className?: string;
   iconClassName?: string;
   valueClassName?: string;
-  titleClassName?: string;
+  subTitleClassName?: string;
 }
 
 const AppStatCard = ({
   icon: Icon,
   value,
-  title,
-  className = "border p-4 rounded-lg border-light-borderGray dark:border-borderGray flex flex-col gap-y-2",
+  mainTitle,
+  mainTitleClassName = "text-light-font01 dark:text-font01 text-sm font-light",
+  subTitle,
+  className = "border p-4 rounded-lg border-light-borderGray dark:border-borderGray flex flex-col gap-y-2 h-fit min-h-40 min-w-[138px]",
   iconClassName = "size-11 text-light-font01 dark:text-font01 font-thin",
   valueClassName = "text-light-font01 dark:text-font01 text-5xl font-semibold",
-  titleClassName = "text-light-font01 dark:text-font01 text-sm font-light",
+  subTitleClassName = "text-light-font01 dark:text-font01 text-sm font-light",
 }: AppStatCardProps) => {
   return (
     <div className={className}>
-      <Icon className={iconClassName} />
+      <div>
+        {Icon && <Icon className={iconClassName} />}
+        {mainTitle && <span className={mainTitleClassName}>{mainTitle}</span>}
+      </div>
       <span className={valueClassName}>{value}</span>
-      <span className={titleClassName}>{title}</span>
+      <span className={subTitleClassName}>{subTitle}</span>
     </div>
   );
 };
