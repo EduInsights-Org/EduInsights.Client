786ab83b77fcb1934979822d7bc007d3e4a1a764
deshan68 <arundeshan@gmail.com>
2025-04-28 12:36:18 +0530

    Feat: add getGPAClassInfo function for GPA classification

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { Role } from "@utils/enums";
+import { GPAClassMapping } from "@utils/types";
 
 export const isIncludedRole = (a: Role[], b: Role[]): boolean => {
   return [...getCrossItems(a, b), ...getCrossItems(b, a)].length > 0;
@@ -16,4 +17,18 @@ export function capitalize(word: string): string {
     .split(" ")
     .map((w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())
     .join(" ");
+}
+
+export function getGPAClassInfo(gpa: number): GPAClassMapping {
+  if (gpa >= 3.7 && gpa <= 4.0) {
+    return { className: "First Class", color: "blue" };
+  } else if (gpa >= 3.3 && gpa <= 3.69) {
+    return { className: "Second Upper Class", color: "sky" };
+  } else if (gpa >= 2.7 && gpa <= 3.29) {
+    return { className: "Second Lower Class", color: "lime" };
+  } else if (gpa >= 2.0 && gpa <= 2.69) {
+    return { className: "Pass Class", color: "amber" };
+  } else {
+    return { className: "Fail", color: "red" };
+  }
 }
