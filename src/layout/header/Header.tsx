f7b372c4a8a48163fa2a93c494b8d09140c8ad9e
deshan68 <arundeshan@gmail.com>
2025-04-30 01:08:33 +0530

    Fix: adjust padding and height in Header component for improved layout

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   const { path, component, drawerTitle, handleOpenDrawer } = useDrawerDetails();
 
   return (
-    <div className="flex w-full items-center justify-between px-5 h-16 gap-x-2">
+    <div className="flex w-full items-center justify-between px-3 h-14 gap-x-2">
       <div className="flex items-center gap-x-3">
         <span className="text-sm font-semibold text-light-font01 dark:text-font01">
           {path}
