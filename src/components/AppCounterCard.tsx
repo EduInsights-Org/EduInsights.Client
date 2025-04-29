20cc99a58559dead3e1aacc78148797f6b33b870
deshan68 <arundeshan@gmail.com>
2025-04-29 11:51:40 +0530

    Fix: update AppStatCard className to use h-full for consistent height

--- src/components/AppCounterCard.tsx
+++ src/components/AppCounterCard.tsx
@@ -16,7 +16,7 @@ const AppStatCard = ({
   mainTitle,
   mainTitleClassName = "text-light-font01 dark:text-font01 text-sm font-light",
   subTitle,
-  className = "border p-4 rounded-lg border-light-borderGray dark:border-borderGray flex flex-col gap-y-2 h-fit min-h-40 min-w-[138px]",
+  className = "border p-4 rounded-lg border-light-borderGray dark:border-borderGray flex flex-col gap-y-2 h-full min-h-40 min-w-[138px]",
   iconClassName = "size-11 text-light-font01 dark:text-font01 font-thin",
   valueClassName = "text-light-font01 dark:text-font01 text-5xl font-semibold",
   subTitleClassName = "text-light-font01 dark:text-font01 text-sm font-light",
