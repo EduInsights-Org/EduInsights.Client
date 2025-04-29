126d585d87ddd9254ba4925845e1dfa7514bfdff
deshan68 <arundeshan@gmail.com>
2025-04-29 11:51:59 +0530

    Feat: enhance Dashboard component to display user count and not verified users

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -7,10 +7,12 @@ import {
   StudentGPA,
 } from "@/slices/resultSlice";
 import { useAppDispatch, useAppSelector } from "@/slices/store";
+import { getUserCount, getUsersByVerificationStatus } from "@/slices/userSlice";
 import { RequestState } from "@/utils/enums";
 import { getGPAClassInfo } from "@/utils/utils";
 import {
   ArrowPathIcon,
+  ExclamationCircleIcon,
   PresentationChartBarIcon,
   SquaresPlusIcon,
   UserGroupIcon,
@@ -27,9 +29,17 @@ const Dashboard = () => {
     (state) => state.user.roleDistribution
   );
   const instituteId = useAppSelector((state) => state.institute.institute.id);
+  const instituteName = useAppSelector(
+    (state) => state.institute.institute.name
+  );
   const studentsGPAs = useAppSelector((state) => state.result.studentsGPAs);
   const resultsStatus = useAppSelector((state) => state.result.status);
   const batchGPAInfo = useAppSelector((state) => state.result.batchGPAInfo);
+
+  const notVerifiedUsers = useAppSelector(
+    (state) => state.user.notVerifiedUsers
+  );
+  const userCount = useAppSelector((state) => state.user.userCount);
 
   const [currentPage, setCurrentPage] = useState<number>(1);
 
@@ -64,13 +74,18 @@ const Dashboard = () => {
   useEffect(() => {
     handleGetStudentsGPAs();
     handleGetBatchAverageGPAs();
+
+    dispatch(
+      getUsersByVerificationStatus({ instituteId, isEmailVerified: false })
+    );
+    dispatch(getUserCount(instituteId));
   }, [instituteId]);
 
   return (
-    <main className="flex flex-col gap-y-2">
+    <main className="flex flex-col gap-y-2 h-full">
       <div className="flex justify-between gap-x-2">
         {/* cards */}
-        <div className="flex w-[15%] flex-col justify-between">
+        <div className="flex w-[15%] flex-col justify-between gap-y-2">
           {/* card one */}
           <AppStatCard
             icon={UserGroupIcon}
@@ -93,7 +108,7 @@ const Dashboard = () => {
         </div>
 
         {/* table */}
-        <div className="border flex flex-col rounded-lg overflow-hidden border-light-borderGray dark:border-borderGray w-[50%] h-[500px]">
+        <div className="border flex flex-col rounded-lg border-light-borderGray dark:border-borderGray w-[50%] h-[500px]">
           {/* table header */}
           <div className="flex gap-x-3 items-center py-4 px-3 bg-light-subBg dark:bg-subBg">
             <button
@@ -126,7 +141,7 @@ const Dashboard = () => {
 
         {/* pie */}
         <div className="border rounded-lg overflow-hidden border-light-borderGray dark:border-borderGray w-[35%] h-[500px] flex flex-col">
-          <div className="flex items-center py-4 px-3 bg-light-subBg dark:bg-subBg">
+          <div className="flex items-center py-4 px-3 bg-light-subBg dark:bg-subBg overflow-auto">
             <button
               onClick={handleGetBatchAverageGPAs}
               className="ml-auto text-xs text-light-font02 dark:text-font02 flex justify-center items-center gap-x-1"
@@ -147,7 +162,7 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="flex justify-between gap-x-2">
+      <div className="flex justify-between gap-x-2 h-full">
         {/* pie chart */}
         <div className="pl-1 border rounded-lg overflow-hidden border-light-borderGray dark:border-borderGray min-w-[250px] w-[40%] flex flex-col">
           <Bar
@@ -163,23 +178,66 @@ const Dashboard = () => {
         </div>
 
         {/* cards */}
-        <div className="w-[60%] flex gap-x-2 justify-between">
-          <AppStatCard
-            icon={PresentationChartBarIcon}
-            subTitle="Current GPA"
-            value={(
-              batchGPAInfo.reduce((acc, item) => acc + item.averageGpa, 0) /
-              batchGPAInfo.length
-            ).toFixed(2)}
-          />
-          {batchGPAInfo.map((item) => (
-            <AppStatCard
-              key={item.batchName}
-              mainTitle={item.batchName}
-              subTitle="Current GPA"
-              value={item.averageGpa.toFixed(2)}
-            />
-          ))}
+        <div className="w-[60%] flex gap-y-2 justify-between flex-col h-full">
+          <div className="w-full border p-3 rounded-lg border-light-borderGray dark:border-borderGray flex items-center gap-x-3 h-fit min-w-[138px]">
+            {/* Current GPA */}
+            <>
+              <PresentationChartBarIcon className="size-12 text-light-font01 dark:text-font01 font-thin" />
+              <div className="flex flex-col">
+                <span className="text-light-font01 dark:text-font01 text-5xl font-semibold">
+                  {(
+                    batchGPAInfo.reduce(
+                      (acc, item) => acc + item.averageGpa,
+                      0
+                    ) / batchGPAInfo.length
+                  ).toFixed(2)}
+                </span>
+                <span className="text-light-font01 dark:text-font01 text-xs font-light leading-3">
+                  {instituteName} Current GPA
+                </span>
+              </div>
+            </>
+            <div className="h-full w-[1px] bg-light-borderGray dark:bg-borderGray mx-auto" />
+            {/* Total Users */}
+            <>
+              <UserGroupIcon className="size-12 text-light-font01 dark:text-font01 font-thin" />
+              <div className="flex flex-col">
+                <span className="text-light-font01 dark:text-font01 text-5xl font-semibold">
+                  {userCount}
+                </span>
+                <span className="text-light-font01 dark:text-font01 text-xs font-light leading-3">
+                  Total Users
+                </span>
+              </div>
+            </>
+            <div className="h-full w-[1px] bg-light-borderGray dark:bg-borderGray mx-auto" />
+            {/* Not Verified Users */}
+            <>
+              <div className="flex flex-col">
+                <div className="flex items-start gap-x-1">
+                  <span className="text-light-font01 dark:text-font01 text-5xl font-semibold">
+                    {notVerifiedUsers.length}
+                  </span>
+                  <ExclamationCircleIcon className="size-4 text-yellow-600" />
+                </div>
+
+                <span className="text-light-font01 dark:text-font01 text-xs font-light leading-3">
+                  Not Verified Users
+                </span>
+              </div>
+            </>
+          </div>
+
+          <div className="w-full h-full flex-row flex gap-x-2 justify-between">
+            {batchGPAInfo.map((item) => (
+              <AppStatCard
+                key={item.batchName}
+                mainTitle={item.batchName}
+                subTitle="Current GPA"
+                value={item.averageGpa.toFixed(2)}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </main>
